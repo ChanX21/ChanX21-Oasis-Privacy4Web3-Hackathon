f2330a54453916b9e571a2e2a82f07abd074f078
Chanchal Delson <chanchaldelson@gmail.com>
2024-10-09 11:49:11 +0530

    contract completion 0.3

--- backend/contracts/PrivaHealth.sol
+++ backend/contracts/PrivaHealth.sol
@@ -13,7 +13,6 @@ contract PrivaHealth is Ownable {
 
     // Struct to represent patient details
     struct Patient {
-        uint256 patientId; // Unique identifier for the patient
         string name; // Patient name (could be hashed for privacy)
         uint256 dateOfBirth; // Patient DOB in UNIX timestamp format
         string gender; // Patient gender
@@ -27,41 +26,60 @@ contract PrivaHealth is Ownable {
         uint256 lastUpdated; // Timestamp of the last update to the patient record
         bool dataSharing; // Whether the patient allows data sharing with R&D facilities
         mapping(address => bool) authorizedDoctors; // Mapping to store authorized doctors
+        mapping(address => bool) authorizedHealthCenters; // Mapping to store authorized health centers for this patient
     }
 
-    // Mapping to store patient records by their unique ID
-    mapping(uint256 => Patient) public patients;
+    struct PatientInfo {
+        address patientAddress; // Address of the patient
+        string name; // Patient name (could be hashed for privacy)
+        uint256 dateOfBirth; // Patient DOB in UNIX timestamp format
+        string gender; // Patient gender
+        string contactInfoHash; // Hash of the patient's contact info (email, phone, etc.)
+        string emergencyContactHash; // Hash of emergency contact info
+        string medicalRecord; // Hash or reference to the off-chain encrypted medical record (e.g., IPFS link)
+        string currentMedications; // Short list of current medications (optional, hashed)
+        string allergies; // Known allergies (optional, hashed)
+        string bloodType; // Patient's blood type
+        // DoctorReview doctorRecommendation;      
+    } 
+
+    // Mapping to store patient records by their address
+    mapping(address => Patient) public patients;
 
     // Mapping to store authorized health centers
     mapping(address => bool) public authorizedHealthCenters;
 
-    // Mapping to store patient addresses
-    mapping(address => uint256) public patientAddresses;
+    // Array to store addresses of patients who have enabled data sharing
+    address[] public dataSharingArray;
 
     // Event to log when a new patient record is added
-    event PatientRecordAdded(uint256 patientId, string name);
+    event PatientRecordAdded(address patientAddress, string name);
 
     // Event to log when a patient record is updated
-    event PatientRecordUpdated(uint256 patientId, string name);
+    event PatientRecordUpdated(address patientAddress, string name);
+
+    // Event to log when data sharing is enabled or disabled
+    event DataSharingChanged(address patientAddress, bool isEnabled);
 
     // Modifier to ensure only authorized entities (doctors or health centers) can modify records
-    modifier onlyAuthorized(uint256 _patientId) {
+    modifier onlyAuthorized(address _patientAddress) {
         require(
-            authorizedHealthCenters[msg.sender] || patients[_patientId].authorizedDoctors[msg.sender],
+            authorizedHealthCenters[msg.sender] || 
+            patients[_patientAddress].authorizedDoctors[msg.sender] ||
+            patients[_patientAddress].authorizedHealthCenters[msg.sender],
             "Not authorized to modify this record"
         );
         _;
     }
 
     // Modifier to ensure only the patient can perform certain actions
-    modifier onlyPatient(uint256 _patientId) {
-        require(patientAddresses[msg.sender] == _patientId, "Only the patient can perform this action");
+    modifier onlyPatient() {
+        require(patients[msg.sender].lastUpdated != 0, "Only the patient can perform this action");
         _;
     }
 
     // Function to add a new patient record
     function addPatientRecord(
-        uint256 _patientId,
         string memory _name,
         uint256 _dateOfBirth,
         string memory _gender,
@@ -72,11 +90,9 @@ contract PrivaHealth is Ownable {
         string memory _allergies,
         string memory _bloodType
     ) public {
-        require(patients[_patientId].patientId == 0, "Patient record already exists");
-        require(patientAddresses[msg.sender] == 0, "Address already associated with a patient");
+        require(patients[msg.sender].lastUpdated == 0, "Patient record already exists");
 
-        Patient storage newPatient = patients[_patientId];
-        newPatient.patientId = _patientId;
+        Patient storage newPatient = patients[msg.sender];
         newPatient.name = _name;
         newPatient.dateOfBirth = _dateOfBirth;
         newPatient.gender = _gender;
@@ -89,83 +105,130 @@ contract PrivaHealth is Ownable {
         newPatient.lastUpdated = block.timestamp;
         newPatient.dataSharing = false;
 
-        patientAddresses[msg.sender] = _patientId;
-
-        emit PatientRecordAdded(_patientId, _name);
+        emit PatientRecordAdded(msg.sender, _name);
     }
 
     // Function to update a patient's record
     function updatePatientRecord(
-        uint256 _patientId,
         string memory _medicalRecord,
         string memory _currentMedications,
         string memory _allergies
-    ) public onlyAuthorized(_patientId) {
-        require(patients[_patientId].patientId != 0, "Patient record does not exist");
+    ) public onlyAuthorized(msg.sender) {
+        require(patients[msg.sender].lastUpdated != 0, "Patient record does not exist");
 
-        Patient storage patient = patients[_patientId];
+        Patient storage patient = patients[msg.sender];
         patient.medicalRecord = _medicalRecord;
         patient.currentMedications = _currentMedications;
         patient.allergies = _allergies;
         patient.lastUpdated = block.timestamp;
 
-        emit PatientRecordUpdated(_patientId, patient.name);
+        emit PatientRecordUpdated(msg.sender, patient.name);
     }
 
     // Function for a patient to authorize a doctor
-    function authorizeDoctor(address _doctorAddress) public {
-        uint256 patientId = patientAddresses[msg.sender];
-        require(patientId != 0, "Patient not registered");
-        patients[patientId].authorizedDoctors[_doctorAddress] = true;
+    function authorizeDoctor(address _doctorAddress) public onlyPatient {
+        patients[msg.sender].authorizedDoctors[_doctorAddress] = true;
     }
 
     // Function for a patient to revoke a doctor's authorization
-    function revokeDoctor(address _doctorAddress) public {
-        uint256 patientId = patientAddresses[msg.sender];
-        require(patientId != 0, "Patient not registered");
-        patients[patientId].authorizedDoctors[_doctorAddress] = false;
+    function revokeDoctor(address _doctorAddress) public onlyPatient {
+        patients[msg.sender].authorizedDoctors[_doctorAddress] = false;
+    }
+
+    // Function for a patient to authorize a health center
+    function authorizeHealthCenter(address _healthCenterAddress) public onlyPatient {
+        patients[msg.sender].authorizedHealthCenters[_healthCenterAddress] = true;
+    }
+
+    // Function for a patient to revoke a health center's authorization
+    function revokeHealthCenter(address _healthCenterAddress) public onlyPatient {
+        patients[msg.sender].authorizedHealthCenters[_healthCenterAddress] = false;
     }
 
     // Function for a patient to set data sharing preferences
-    function setDataSharing(bool _allowSharing) public {
-        uint256 patientId = patientAddresses[msg.sender];
-        require(patientId != 0, "Patient not registered");
-        patients[patientId].dataSharing = _allowSharing;
+    function setDataSharing(bool _allowSharing) public onlyPatient {
+        patients[msg.sender].dataSharing = _allowSharing;
+        
+        if (_allowSharing) {
+            // Add patient address to dataSharingArray if not already present
+            bool isPresent = false;
+            for (uint i = 0; i < dataSharingArray.length; i++) {
+                if (dataSharingArray[i] == msg.sender) {
+                    isPresent = true;
+                    break;
+                }
+            }
+            if (!isPresent) {
+                dataSharingArray.push(msg.sender);
+            }
+        } else {
+            // Remove patient address from dataSharingArray
+            for (uint i = 0; i < dataSharingArray.length; i++) {
+                if (dataSharingArray[i] == msg.sender) {
+                    dataSharingArray[i] = dataSharingArray[dataSharingArray.length - 1];
+                    dataSharingArray.pop();
+                    break;
+                }
+            }
+        }
+
+        emit DataSharingChanged(msg.sender, _allowSharing);
     }
 
-    // Function to retrieve a patient record by ID (only public, non-sensitive data)
-    function getPatientRecord(uint256 _patientId)
+    // Function to retrieve patient records (only public, non-sensitive data)
+    function getPatientRecords()
         public
         view
+        returns (
+            PatientInfo[] memory
+        )
+    {   
+        PatientInfo[] memory patientInfo = new PatientInfo[](dataSharingArray.length);
+        for (uint256 i = 0; i < dataSharingArray.length; i++) {
+            Patient storage p = patients[dataSharingArray[i]];
+            patientInfo[i] = PatientInfo({
+                patientAddress: dataSharingArray[i],
+                name: p.name,
+                dateOfBirth: p.dateOfBirth,
+                gender: p.gender,
+                contactInfoHash: p.contactInfoHash,
+                emergencyContactHash: p.emergencyContactHash,
+                medicalRecord: p.medicalRecord,
+                currentMedications: p.currentMedications,
+                allergies: p.allergies,
+                bloodType: p.bloodType
+            });
+        }
+
+        return patientInfo;
+    }
+
+    // Function to retrieve sensitive patient data (only for authorized entities)
+    function getSensitivePatientData(address _patientAddress)
+        public
+        view
+        onlyAuthorized(_patientAddress)
         returns (
             string memory name,
             uint256 dateOfBirth,
             string memory gender,
             string memory bloodType,
             uint256 lastUpdated,
-            bool dataSharing
-        )
-    {   
-        Patient storage p = patients[_patientId];
-        require(p.patientId != 0, "Patient record does not exist");
-        require(patients[_patientId].authorizedDoctors[msg.sender], "Only the authorized doctor can call this function");
-        return (p.name, p.dateOfBirth, p.gender, p.bloodType, p.lastUpdated, p.dataSharing);
-    }
-
-    // Function to retrieve sensitive patient data (only for authorized entities)
-    function getSensitivePatientData(uint256 _patientId)
-        public
-        view
-        onlyAuthorized(_patientId)
-        returns (
+            bool dataSharing,
             string memory medicalRecord,
             string memory currentMedications,
             string memory allergies
         )
     {
-        Patient storage p = patients[_patientId];
-        require(p.patientId != 0, "Patient record does not exist");
-        return (p.medicalRecord, p.currentMedications, p.allergies);
+        Patient storage p = patients[_patientAddress];
+        require(p.lastUpdated != 0, "Patient record does not exist");
+        require(
+            authorizedHealthCenters[msg.sender] || 
+            p.authorizedDoctors[msg.sender] ||
+            p.authorizedHealthCenters[msg.sender],
+            "Not authorized to access sensitive data"
+        );
+        return (p.name, p.dateOfBirth, p.gender, p.bloodType, p.lastUpdated, p.dataSharing, p.medicalRecord, p.currentMedications, p.allergies);
     }
 
     // Function to add or remove authorized health centers (should be restricted to admin in a real-world scenario)
